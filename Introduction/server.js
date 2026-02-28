e84e8588bc496963d63f47239229de7d6d078595
kachuaofficial <shivammisra2021@gmail.com>
2026-03-01 00:15:38 +0530

    Refactor code structure for improved readability and maintainability also added a new mini project notes api

---
+++ Introduction/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+app.listen(3000, () => {
+    console.log("Server is running on port " + 3000);
+
+})
+
+app.get('/', (req, res) => {
+    res.send("hello")
+})
+
+
+
+app.get('/about',(req, res) => {
+    res.send("About page")
+})
---
+++ NotesApi/server.js
@@ -0,0 +1,8 @@
+//Start server here
+
+const app = require("./src/app")
+
+app.listen(3000, () => {
+    console.log("server started");
+    
+})
